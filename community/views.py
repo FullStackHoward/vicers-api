64311ad3ef4e47e76a0c1b740f065d1703d3a2f3
Joshua Howard <josh@mrhoward.net>
2026-03-13 15:44:51 -0400

    Updated events API view queryset to only return events that are today or in the future.

--- community/views.py
+++ community/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,10 +10,28 @@ class EventListView(generics.ListAPIView):
     serializer_class = EventSerializer
 
     def get_queryset(self):
+        today = timezone.now().date()
         community = self.request.query_params.get('community', None)
+        qs = Event.objects.filter(start_time__date__gte=today).order_by('start_time')[:5]
         if community:
-            return Event.objects.filter(community=community)
-        return Event.objects.all()
+            qs = Event.objects.filter(
+                start_time__date__gte=today,
+                community=community,
+            ).order_by('start_time')[:5]
+        return qs
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        if not queryset:
+            return Response(
+                {
+                    'message': 'No events are scheduled at this time',
+                    'results': [],
+                },
+                status=status.HTTP_200_OK,
+            )
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 class AnnouncementView(generics.ListAPIView):
     serializer_class = AnnouncementSerializer
