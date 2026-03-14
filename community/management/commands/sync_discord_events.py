9813bab23f9dc6333cac225472a30969e602ee44
Joshua Howard <josh@mrhoward.net>
2026-03-14 16:56:28 -0400

    updated sync_discord_events.py with a cleanup process to removed deleted events

--- community/management/commands/sync_discord_events.py
+++ community/management/commands/sync_discord_events.py
@@ -38,14 +38,27 @@ def save_event(discord_event_id, title, description, start_time, end_time, commu
     )
     return event, created
 
+@sync_to_async
+def delete_removed_events(community, active_discord_ids):
+    deleted, _ = Event.objects.filter(
+        community=community
+    ).exclude(
+        discord_event_id__in=active_discord_ids
+    ).delete()
+    if deleted:
+        print(f'Deleted {deleted} removed event(s) for {community}')
+
 async def sync_events():
     async with aiohttp.ClientSession() as session:
         for guild_id, community in GUILD_COMMUNITY_MAP.items():
             print(f'Syncing events for {community}...')
             events = await fetch_guild_events(session, guild_id)
 
+            active_discord_ids = []
+
             for discord_event in events:
                 discord_event_id = str(discord_event['id'])
+                active_discord_ids.append(discord_event_id)
                 title = discord_event['name']
                 description = discord_event.get('description', '')
                 start_time = datetime.fromisoformat(
@@ -70,6 +83,8 @@ async def sync_events():
                     print(f'Created event: {title} ({community})')
                 else:
                     print(f'Updated event: {title} ({community})')
+
+            await delete_removed_events(community, active_discord_ids)
 
 class Command(BaseCommand):
     help = 'Syncs scheduled events from Discord servers into the database'
